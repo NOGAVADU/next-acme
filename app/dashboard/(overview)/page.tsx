2b5f8eb0e2eb6cf9ed416422f73ff6cce9fc5dd4
Даниил Мосолов <dkuets@gmail.com>
2024-03-24 17:52:08 +0300

    to 14 chapter

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,4 @@
-import CardWrapper, {Card} from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import {lusitana} from '@/app/ui/fonts';
